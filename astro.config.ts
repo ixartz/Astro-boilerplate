a045e65b2bc1f68b78ce27218820a584053462d5
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-04 01:32:27 +0200

    feat: add robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  integrations: [react(), tailwind({}), sitemap()],
+  // Important!
+  // Only official '@astrojs/*' integrations are currently supported by Astro.
+  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
+  // with 'astro build' command.
+  experimental: {
+    integrations: true,
+  },
+  integrations: [react(), tailwind({}), sitemap(), robotsTxt()],
 });
