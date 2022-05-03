81b2ff0f5b893f40b4deb9d8b123ce65fd89f167
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-04 01:18:59 +0200

    feat: add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
-  integrations: [react(), tailwind({})],
+  integrations: [react(), tailwind({}), sitemap()],
 });
