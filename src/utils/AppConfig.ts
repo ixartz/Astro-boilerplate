2af6eed6552b885742ae3e1f28da3d7f2170a28f
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-10 17:42:19 +0200

    refactor: add AppConfig for easy customization

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,7 @@
+export const AppConfig = {
+  site_name: 'Astro boilerplate',
+  title: 'Astro boilerplate',
+  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  locale_region: 'en-us',
+  locale: 'en'
+};
