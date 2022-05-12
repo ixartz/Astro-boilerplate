7f81618787e6a57926c9047602cfcb3a8e840a59
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-12 23:43:30 +0200

    feat: implement one project card

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,11 +1,18 @@
 import type { ReactNode } from 'react';
 
 type ISectionProps = {
+  title?: ReactNode;
   children: ReactNode;
 };
 
 const Section = (props: ISectionProps) => (
-  <div className="mx-auto max-w-screen-lg px-3 py-5">{props.children}</div>
+  <div className="mx-auto max-w-screen-lg px-3 py-5">
+    {props.title && (
+      <div className="mb-6 text-2xl font-bold">{props.title}</div>
+    )}
+
+    {props.children}
+  </div>
 );
 
 export { Section };
--- src/layouts/LandingPage.tsx
+++ src/layouts/LandingPage.tsx
@@ -1,3 +1,6 @@
+import { Section } from '@/components/Section';
+import projectWebDesign from '@/images/project-web-design.png';
+
 import { Hero } from './Hero';
 import { Navbar } from './Navbar';
 
@@ -5,6 +8,28 @@ const LandingPage = () => (
   <>
     <Navbar />
     <Hero />
+
+    <Section title="Recent Projects">
+      <div className="flex items-center gap-8">
+        <div className="shrink-0">
+          <img
+            className="h-56 w-56"
+            src={projectWebDesign}
+            alt="Project Web Design"
+          />
+        </div>
+
+        <div>
+          <div className="text-xl font-semibold">Project 1</div>
+
+          <p className="mt-3 text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            bibendum. Nunc non posuere consectetur, justo erat semper enim, non
+            hendrerit dui odio id enim.
+          </p>
+        </div>
+      </div>
+    </Section>
   </>
 );
 
