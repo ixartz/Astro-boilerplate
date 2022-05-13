2ed715b631d39441a8efd1f8b813c3c1b9ec2416
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-13 22:16:37 +0200

    refactor: migrate from index astro to blog gallery

--- src/layouts/BlogGallery.tsx
+++ src/layouts/BlogGallery.tsx
@@ -1,7 +1,15 @@
+import type { MarkdownInstance } from 'astro';
+import { format } from 'date-fns';
+
 import { Section } from '@/components/Section';
 import imagePost from '@/images/image-post.jpeg';
+import type { IFrontmatter } from '@/types/IFrontmatter';
 
-const BlogGallery = () => (
+type IBlogGalleryPosts = {
+  postList: MarkdownInstance<IFrontmatter>[];
+};
+
+const BlogGallery = (props: IBlogGalleryPosts) => (
   <Section
     title={
       <div className="flex items-baseline justify-between">
@@ -13,27 +21,30 @@ const BlogGallery = () => (
       </div>
     }
   >
-    <div className="grid grid-cols-3 gap-3">
-      <div className="overflow-hidden rounded-md bg-slate-800">
-        <div className="aspect-w-3 aspect-h-2">
-          <img
-            className="h-full w-full object-cover object-center"
-            src={imagePost}
-            alt="alt text"
-          />
-        </div>
+    <div className="grid grid-cols-3 gap-6">
+      {props.postList.map((elt) => (
+        <a key={elt.url} href={elt.url}>
+          <div className="overflow-hidden rounded-md bg-slate-800">
+            <div className="aspect-w-3 aspect-h-2">
+              <img
+                className="h-full w-full object-cover object-center"
+                src={imagePost}
+                alt="alt text"
+              />
+            </div>
 
-        <div className="px-3 pt-4 pb-6 text-center">
-          <h2 className="text-xl font-semibold">Lorem ipsum dolor</h2>
+            <div className="px-3 pt-4 pb-6 text-center">
+              <h2 className="text-xl font-semibold">{elt.frontmatter.title}</h2>
 
-          <div className="mt-1 text-xs text-gray-400">April 24, 2022</div>
+              <div className="mt-1 text-xs text-gray-400">
+                {format(new Date(elt.frontmatter.publishDate), 'LLL d, yyyy')}
+              </div>
 
-          <div className="mt-2 text-sm">
-            Nunc non posuere consectetur, justo erat semper enim, non hendrerit
-            dui odio id enim.
+              <div className="mt-2 text-sm">{elt.frontmatter.description}</div>
+            </div>
           </div>
-        </div>
-      </div>
+        </a>
+      ))}
     </div>
   </Section>
 );
