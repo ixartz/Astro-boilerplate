0419c9113a31d86f40e332ffb808cb5c0b58efd8
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-14 03:44:53 +0200

    feat: improve UI in pagination

--- src/components/NewerOlderPagination.tsx
+++ src/components/NewerOlderPagination.tsx
@@ -5,9 +5,9 @@ type INewerOlderPaginationProps = {
 };
 
 const NewerOlderPagination = (props: INewerOlderPaginationProps) => (
-  <div className="flex justify-between">
-    {props.page.url.prev && <a href={props.page.url.prev}>Newer Posts</a>}
-    {props.page.url.next && <a href={props.page.url.next}>Older Posts</a>}
+  <div className="flex justify-center gap-8">
+    {props.page.url.prev && <a href={props.page.url.prev}>← Newer Posts</a>}
+    {props.page.url.next && <a href={props.page.url.next}>Older Posts →</a>}
   </div>
 );
 
