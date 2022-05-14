a6fa7867342afdd32329410b389b23bc63365307
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-14 03:39:49 +0200

    feat: clean up in posts/ page

--- src/components/NewerOlderPagination.tsx
+++ src/components/NewerOlderPagination.tsx
@@ -1,14 +1,14 @@
 import type { FrontmatterPage } from '@/types/IFrontmatter';
 
-type IPaginationProps = {
+type INewerOlderPaginationProps = {
   page: FrontmatterPage;
 };
 
-const Pagination = (props: IPaginationProps) => (
+const NewerOlderPagination = (props: INewerOlderPaginationProps) => (
   <div className="flex justify-between">
     {props.page.url.prev && <a href={props.page.url.prev}>Newer Posts</a>}
     {props.page.url.next && <a href={props.page.url.next}>Older Posts</a>}
   </div>
 );
 
-export { Pagination };
+export { NewerOlderPagination };
