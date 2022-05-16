c916e19ee99b882cede020d02daa1bf700a3d623
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-16 19:48:47 +0200

    feat: add demo folder in base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { astroImageTools } from 'astro-imagetools';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/astro-boilerplate', // Set a path prefix.
+  base: '/demo/astro-boilerplate', // Set a path prefix.
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
   // Important!
   // Only official '@astrojs/*' integrations are currently supported by Astro.
