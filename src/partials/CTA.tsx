a5135b33538f7f415295a703c4f920c6f6b7fc06
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-16 15:27:52 +0200

    feat: make newsletter component responsive

--- src/partials/CTA.tsx
+++ src/partials/CTA.tsx
@@ -3,8 +3,8 @@ import { Section } from '@/components/Section';
 
 const CTA = () => (
   <Section>
-    <div className="flex flex-row items-center justify-between gap-x-6">
-      <div className="w-7/12">
+    <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+      <div className="sm:w-7/12">
         <div className="text-3xl font-bold">
           Subscribe to my <GradientText>Newsletters</GradientText>
         </div>
@@ -15,7 +15,7 @@ const CTA = () => (
         </p>
       </div>
 
-      <div className="w-5/12">
+      <div className="w-full sm:w-5/12">
         <form className="flex rounded-full bg-slate-800 px-4 py-2 focus-within:ring-2 focus-within:ring-cyan-600 hover:ring-2 hover:ring-cyan-600">
           <input className="w-full appearance-none bg-slate-800 focus:outline-none" />
 
