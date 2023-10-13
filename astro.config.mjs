bec033dec2de46d142f010489591129976e8602a
Javier Ledezma <juls0593@gmail.com>
2023-10-12 19:28:40 -0600

    feat: upgrade astro to version 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,13 +10,6 @@ export default defineConfig({
   // base: '.', // Set a path prefix.
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
   trailingSlash: 'always', // Use to always append '/' at end of url
-  // Important!
-  // Only official '@astrojs/*' integrations are currently supported by Astro.
-  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
-  // with 'astro build' command.
-  experimental: {
-    integrations: true,
-  },
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
